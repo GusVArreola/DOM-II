1ad84f1212b80b42d47d292b3c73bcb6eb0724ac
Gustavo Vega <gustavov.arreola@gmail.com>
2022-07-14 02:30:38 -0600

    Completed Unit 2 Week 1 Module 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//load
+window.onload = function (evt) {
+    console.log(` event ${evt.type} fired! Ready to go!`);
+    const heading = document.querySelector("h1");
+    heading.textContent = "READY TO GO !!";
+  
+//copy
+    window.addEventListener("copy", () => {
+      navigator.clipboard.readText().then((text) => {
+        heading.textContent += text;
+      });
+    });
+//click
+  
+    document.body.addEventListener("click", (evt) => {
+      evt.target.classList.toggle("mirror");
+    });
+  
+//dblclick
+    document.body.addEventListener("dblclick", (evt) => {
+      evt.target.innerHTML = "";
+    });
+  
+//keydown
+  
+    window.addEventListener("keydown", (evt) => {
+      if (evt.key == 6) {
+        document.body.innerHTML = "<h1>Welcome To Fun Bus!</h1>";
+      }
+    });
+  
+//mousemove
+  
+    document.body.addEventListener("mousemove", (evt) => {
+      const { clientX, clientY } = evt;
+      console.log(`mouse is at ${clientX},${clientY}`);
+    });
+}
